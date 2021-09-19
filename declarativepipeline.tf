b8687a387dc871685999c74170a533a2b95b127c
sravan <burrisravanreddy@gmail.com>
2021-09-19 12:09:40 +0100

    SECOND

--- declarativepipeline.tf
+++ declarativepipeline.tf
@@ -25,7 +25,62 @@ pipeline {
     }
 }
 
+###########################2nd timestamp or when statement #####################################
+
+pipeline {
+    agent any
+
+    options {
+        timestamps()
+    }
+
+    stages {
+        stage("Build") {
+            options {
+                timeout(time: 1, unit: "MINUTES")
+            }
+            steps {
+                sh 'printf "\\e[31mSome code compilation here...\\e[0m\\n"'
+            }
+        }
+
+        stage("Test") {
+            when {
+                environment name: "ENVT", value: "TEST"
+            }
+            options {
+                timeout(time: 2, unit: "MINUTES")
+            }
+            steps {
+                echo "This Is An Test Environment"
+            }
+        }
+
+        stage("Dev") {
+            when {
+                environment name: "ENVT", value: "DEV"
+            }
+            options {
+                timeout(time: 2, unit: "MINUTES")
+            }
+            steps {
+                echo "This Is An Dev Environment"
+            }
+        }
+
+        stage("Prod") {
+            when {
+                environment name: "ENVT", value: "PROD"
+            }
+            options {
+                timeout(time: 2, unit: "MINUTES")
+            }
+            steps {
+                echo "This Is An Prod Environment"
+            }
+        }
+    }
+}
 
 
-###########################2nd level#####################################
 
